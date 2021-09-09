9f8d4de8b512c06232153c40a65111e0cca6470d
Gogyungho <whohow10@gmail.com>
2021-09-10 02:20:47 +0900

    module

---
+++ 10-javascript/10-3-module2.js
@@ -0,0 +1,5 @@
+// import sum, { print as printMessage }from './10-3-module1.js';
+import * as calculator from './10-3-module1.js';
+console.log(calculator.add(2,4))
+calculator.print();
+calculator.number;
